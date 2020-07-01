b3db345c34b3c3a70461d3f8f2ac2a3a5616d29c
Eduardo Marques dos Reis <54919290+EduardoMdR@users.noreply.github.com>
2020-07-01 16:41:48 -0300

    song - mp3

--- db/migrate/20200630023936_create_song_playlists.rb
+++ db/migrate/20200630023936_create_song_playlists.rb
@@ -1,8 +1,8 @@
 class CreateSongPlaylists < ActiveRecord::Migration[5.2]
   def change
     create_table :song_playlists do |t|
-      t.references :song, foreign_key: true
-      t.references :playlist, foreign_key: true
+      t.integer :song_id
+      t.integer :playlist_id
 
       t.timestamps
     end
