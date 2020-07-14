5471f58f64beb2db975403050f15ee88a2dc0287
Pedro Venzi <pvenzi.pv@gmail.com>
2020-07-14 16:01:39 -0300

    aprimorando a homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#teste"
+  root "application#homepage"
   resources :songs
   resources :albums
   resources :favoritesongs
